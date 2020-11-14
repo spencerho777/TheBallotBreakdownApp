b4882ff9f48e5a2d7f001fa4f2e4b90a50b53340
Spencer <spencerho777@yahoo.com>
2020-11-14 00:14:58 -0800

    Initial Commit

---
+++ PoliticalAccountability/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PoliticalAccountability
+//
+//  Created by Van Nguyen on 11/14/20.
+//  Copyright © 2020 Spencer Ho's Hose. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
